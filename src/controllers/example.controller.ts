3fd3232c6a8bc823ccb05f3613cb0949375eb343
modstam <modstam@live.com>
2023-10-26 10:36:26 +0200

    add mongodb docker

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
 import { assertRequestData } from 'src/assertions/exampleDataAssertion';
-import { insertData } from 'src/services/mongodb.service';
+import { insertData, getAllData } from 'src/services/mongodb.service';
 
 const router = Router();
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   const data = assertRequestData(req.body);
-  const result = insertData(data);
+  const result = await insertData(data);
   res.status(201).json(result);
+});
+
+router.get('/all', async (req, res, next) => {
+  const result = await getAllData();
+  res.status(200).json(result);
 });
 
 export default router;
--- src/services/mongodb.service.ts
+++ src/services/mongodb.service.ts
@@ -4,11 +4,12 @@ export interface ExampleData {
   message: string;
 }
 
-const user = process.env.DB_USER || 'localUser';
-const password = process.env.DB_PASSWORD || '1234';
+// const user = process.env.DB_USER || 'localUser';
+// const password = process.env.DB_PASSWORD || '1234';
 const host = process.env.DB_HOST || 'localhost:27017';
 
-const url = `mongodb://${user}:${password}@${host}`;
+// const url = `mongodb://${user}:${password}@${host}`;
+const url = `mongodb://${host}`;
 const dbName = 'data';
 
 const collection = 'data';
@@ -22,4 +23,14 @@ export const dbClose = () => client.close();
 export const insertData = async (data: ExampleData): Promise<ExampleData> => {
   await db.collection<ExampleData>(collection).insertOne(data);
   return { ...data };
+};
+
+export const getAllData = async (): Promise<ExampleData[]> => {
+  const cursor = await db.collection<ExampleData>(collection).find({});
+
+  const result: ExampleData[] = [];
+  for await (const data of cursor) {
+    result.push(data);
+  }
+  return { ...result };
 };
